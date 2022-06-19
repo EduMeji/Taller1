f34685d77028ac108d1855b1b0dd147aedb51b73
EduMeji <edu.jor14@gmail.com>
2022-06-19 18:55:58 -0500

    Pagina web primer commit

---
+++ formulario.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+
+<body class="bg-info">
+   <?php
+      include_once "menu.php";
+  ?>
+  <br>
+  <center><h1 class=" col-8 bg-dark text-white">Formulario datos personales</h1></center>
+ <form class="row g-3">
+
+  <div class="col-md-3">
+    <label for="nombre" class="form-label">Nombre</label>
+    <input type="text" name="nombre" id="nombre"
+     onkeypress="return validar(event)" class="form-control" maxlength="25" placeholder="Ingrese nombres" required>
+  </div>
+
+  <div class="col-md-3">
+    <label for="apellido" class="form-label">Apellido</label>
+    <input type="text" class="form-control" maxlength="25" id="apellido" onkeypress="return validar(event)" value="" placeholder="Ingrese apellidos"required>
+  </div>
+  <div class="col-md-3">
+    <label for="correo" class="form-label">Correo Electronico</label>
+    <div class="input-group">
+      <span class="input-group-text" id="inputGroupPrepend2">@</span>
+      <input type="e-mail" class="form-control" id="correo"  aria-describedby="inputGroupPrepend2" required>
+    </div>
+  </div>
+  <div class="col-md-3">
+    <label for="direcion" class="form-label">Direcion</label>
+    <input type="text" class="form-control" id="direcion" onkeypress="return validar(event)" required>
+  </div>
+  <div class="col-md-3">
+    <label for="validationDefault04" class="form-label">Estado civil</label>
+    <select class="form-select" id="validationDefault04" required>
+      <option selected disabled value="">Opciones</option>
+      <option>Soltero</option>
+      <option>Casado</option>
+      <option>Viudo</option>
+      <option>Divorsiado</option>
+    </select>
+  </div>
+  <div class="col-md-3">
+    <label for="fecha_n" class="form-label">Fecha de nacimiento</label>
+    <input type="date" class="form-control" id="fecha_n" required>
+  </div>
+  <div class="col-md-3">
+    <label for="telefono" class="form-label">Telefono</label>
+    <input type="number" maxlength="10" class="form-control" id="telefono" required>
+  </div>
+  <div class="col-12">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+      <label class="form-check-label" for="invalidCheck2">
+        Acepto los términos y condiciones
+      </label>
+    </div>
+  </div>
+
+  <div class="col-12">
+    <button class="btn btn-primary" type="submit">Aceptar</button>
+  </div>
+</form>
+</body>
+</html>
---
+++ misionyvision.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mision y Vision</title>
+</head>
+<body class="">
+  <?php 
+    include_once "menu.php";
+  ?>
+  <h1 class="">Misión</h1>
+  <br>
+  <p class="">
+    Buscamos satisfacer la necesidad de los clientes a través de un producto, el compromiso, respeto, responsabilidad, calidad y calidez, perfeccionar nuestros procesos, a través de una mejora continua, para lograr eficacia, eficiencia y productividad buscando alcanzar la excelencia, en beneficio y satisfacción de nuestros clientes, la mejor calidad de automoviles, camionetas, camiones y motos, la calidad de nuestros productos que ofrecemos.
+  </p>
+  <br>
+  <h1 class="">Visión</h1>
+  <br>
+  <p class="">
+    Al 2022, ser la mejor concesionaria en el sector con nuestros productos, la mejor calidad de automoviles, camionetas, camiones y motos, la calidad de nuestros productos que ofrecemos, perfeccionar nuestros procesos, a través de una mejora continua, para lograr eficacia, eficiencia y productividad buscando alcanzar la excelencia, en beneficio y satisfacción de nuestros clientes, el compromiso, respeto, responsabilidad, calidad y calidez.
+  </p>
+>
+</html>
