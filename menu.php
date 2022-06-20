80b2ec2f01092acf96dafc92f903b7e74c520b1b
EduMeji <edu.jor14@gmail.com>
2022-06-19 19:37:07 -0500

    Pagina web quinto commit

--- menu.php
+++ menu.php
@@ -31,9 +31,6 @@
         <li class="nav-item">
           <a class="nav-link  text-white" href="registro_clientes.php">Registro de Clientes</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link  text-white" href="directorio_ejecutivo.php">Directorio Ejecutivo</a>
-        </li>
       </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="buscar" aria-label="Search">
