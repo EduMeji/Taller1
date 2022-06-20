1079b72d40af03da222014d251ed0fb64996103e
EduMeji <edu.jor14@gmail.com>
2022-06-19 19:11:13 -0500

    Pagina web primer commit

--- index.php
+++ index.php
@@ -7,6 +7,30 @@
     <title>Inicio</title>
 </head>
 <body>
-    <h1>Bienvenidos</h1>
+	<h1>Bienvenidos a nuestra concesionaria</h1>
+	<br>
+	<p>
+		Nuestra concesionaria les ofrece una variadead de vehiculos, usted puede escoger
+		desde automoviles hasta camiones, tenemos de diferentes modelos, marcas, precios,
+		colores, somos una de las mejores concesionarias ya que tenemos vehiculos de todo
+		tipo. 
+	</p>
+	<div align="center">
+		<img src="img/cliente.jpg">
+	</div>	
+	<br>
+	<p>
+		Te felicitamos por formar parte de esta gran familia y nos ponemos a tu disposición para brindarte el mejor servicio, la mejor tecnología, mano de obra especializada y sobre todo seguridad para que vivas el Passion for Life en conjunto con tu nuevo Vehiculo.
+	</p>
+	<div align="center">
+		<img src="img/bienvenida.jpg">
+	</div>
+	<br>
+	<p>
+		Te acompañamos dentro de esta nueva experiencia y recuerda que solo en esta pagina web vas a poder encontrar los mejores vehiculos, te ofrecemos vehiculos de toda clase desde automoviles hasta camiones de todo precio, marca, combustible, automaticoas, caja de cambios, electricos.	
+	</p>
+	<div align="center">
+		<img  src=img/elecom.jpg">
+	</div>
 </body>
 </html>
